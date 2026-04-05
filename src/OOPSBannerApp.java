22c3de2770298585fd1b75b6642a5d951a5503f2
Akshay Harikumar <your-email@example.com>
2026-04-06 01:02:36 +0530

    UC2: Banner OOPS implemented

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,14 +1,14 @@
-/**
- * OOPS Banner Application
- * UC1 - Print OOPS
- * @author Akshay
- * @version 1.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   ***** ");
+
+    }
 }
